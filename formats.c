65b01892d91fca346610351ac2f1ce153992150e
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-25 14:59:50 +0100

    add file _putchar.c and update file formats.c

--- formats.c
+++ formats.c
@@ -65,3 +65,40 @@ int print_mod(void)
 {
 	return (write(1, "%", 1));
 }
+
+/**
+* print_int - integer to be printed
+* @n: number given by user
+*
+* Return: the number to be printed
+*/
+int print_number(int n)
+{
+	int num;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (n);
+	}
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		num = -n;
+	}
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (num / 10)
+			{
+				print_number(num / 10);
+				_putchar((num % 10) + '0');
+			}
+		}
+	}
+
+	return (0);
+}
