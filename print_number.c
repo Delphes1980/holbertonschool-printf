0df010a98b2472eb30ae94b8f3c3539b58fdb322
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-03-26 10:43:56 +0100

    modify print_number function

---
+++ print_number.c
@@ -0,0 +1,34 @@
+
+/**
+* print_number - integer to be printed
+* @n: the numbers
+*
+* Return: The number printed
+*/
+int print_number(int n)
+{
+	unsigned int num;
+	int count = 0;
+
+	if (n == 0)
+	return (n);
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		count++;
+		num = -n;
+	}
+
+	else
+	{
+	num = n;
+	}
+
+	if (num / 10)
+	count += print_number(num / 10);
+
+_putchar((num % 10) + '0');
+
+return (count);
+}
